00e8dd64af943e1a243cd578c91891ad6abce889
Garon Davis <garoncdavis@gmail.com>
2015-10-27 10:36:56 -0400

    moved middleware to module file

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+var middleWare = require('./modules/middleware.js')
 
-var middleWare = {
-    requireAuthentication: function (req, res, next) {
-        console.log('private route hit!');
-        next();
-    },
-    logger: function (req, res, next) {
-
-        var date = new Date().toString();
-        console.log('Request: ' + req.method + ' ' + req.originalUrl + ' date: ' + date);
-        next();
-    }
-};
 
 app.use(middleWare.logger)
 
